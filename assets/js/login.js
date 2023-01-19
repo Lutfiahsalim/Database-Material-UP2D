d5081ca6ff618743340b0219544648fd39cf955e
Lutfiahsalim <Lutfiah.salim12@gmail.com>
2023-01-19 17:05:19 +0800

    login

---
+++ assets/js/login.js
@@ -0,0 +1,162 @@
+const baseUrl = "https://newbackend-datar-zbzgct5ujq-et.a.run.app"
+
+// import swal from 'sweetalert';
+
+// const formLogin = {
+//     username: document.getElementById('username'),
+//     password: document.getElementById('password'),
+//     login: document.getElementById('btnLogin')
+// }
+
+$(document).ready(function () {
+    $("#btnLogin").on('click', function (e) {
+        e.preventDefault();
+        login();
+    })
+})
+
+function ajaxFailedFeedback(response) {
+    Swal.fire({
+        title: "Login Gagal",
+        text: response.responseJSON.msg,
+        icon: "error",
+    })
+    if (response.code == 404 ) {
+        Swal.fire()
+    }
+    else if (response.code == 400) {
+        Swal.fire()
+    }
+}
+
+function login() {
+    const username = $("#username").val();
+    const password = $("#password").val();
+
+    var setupLogin = {
+        "url": baseUrl+ "/login",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json",
+        },
+        "data": JSON.stringify({
+          "username": username,
+          "password": password
+        }),
+      };
+      
+      $.ajax(setupLogin)
+      .fail(function(response) {
+        console.log(response)
+        ajaxFailedFeedback(response) 
+        })
+      .done(function (response) {
+        console.log(response)
+            if (response.status_code == 200) {
+                const dataToken = JSON.stringify({
+                    token: response.data.token
+                    // refreshToken: response.refreshToken
+                });
+                const userInfo = JSON.stringify({
+                    username: response.data.user.username,
+                    role: response.data.user.role
+                });
+                sessionStorage.setItem("TOKEN_DATA", dataToken);
+                sessionStorage.setItem("INFO_USER", userInfo);
+                
+                // let temp = parseJSON(sessionStorage.getItem("TOKEN_DATA"));
+                let temp = JSON.parse(sessionStorage.getItem("INFO_USER"));
+                console.log(temp)
+                // user = $.parseJSON(temp);
+
+                // $('#btnLogin' ).removeClass( "disable").prop('disabled', false);
+                Swal.fire({
+                    title: `${temp.username} login ke datar!`,
+                    timer: 1500,
+                    icon: "success",
+                    showCancelButton: false,
+                    showConfirmButton: false,
+                }).then(function () {
+                    let role = response.data.user.role;
+                        if (role == "adminpro") {
+                            window.location.href = "index.html";
+                        } else if (role == "admin") {
+                            window.location.href = "detail.html";
+                        } else if (role == "user") {
+                            window.location.href = "detail.html";
+                        }
+                });
+            } 
+        })       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //         console.log(response)
+    //             if (response.code("200") {
+    //                 const dataToken = JSON.stringify({
+    //                     token: response.token,
+    //                     refreshToken: response.refreshToken 
+    //                 })
+    //                 const userInfo = JSON.stringify({
+    //                     username: kapital(response.userData.username),
+    //                     role: kapital(response.userData.role)
+    //                 })
+    //                 sessionStorage.setItem("TOKEN_DATA", dataToken);
+    //                 sessionStorage.setItem("INFO_USER", userInfo);
+
+    //                 let temp = sessionStorage.getItem("INFO_USER");
+    //                 user = $.parseJSON(temp);
+    //             });
+    //         });
+            
+    //     });
+        
+    // }
+    
+    
+                    // if (response.status == "Success") {
+                    //     swal({
+                    //         type: response.status,
+                    //         title: "Berhasil!",
+                    //         text: `${user.username} login ke datar!`,
+                    //         icon: "success",
+                    //     });
+                    // } 
+                    // else (response.status == ajaxFailedFeedback)
+// swal({
+//     title: "Gagal!",
+//     text: "Anda tidak bisa login",
+//     icon: "error",
+//     buttons: true,
+//     dangerMode: true,
+//   });
+// }
