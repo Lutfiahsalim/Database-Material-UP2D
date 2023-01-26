b8fb5fc64ef85d3375ae50fa80544f073820e2dd
Lutfiahsalim <Lutfiah.salim12@gmail.com>
2023-01-26 18:07:45 +0800

    error calldata

---
+++ assets/js/calldata.js
@@ -0,0 +1,208 @@
+const baseUrl = "https://newbackend-datar-zbzgct5ujq-et.a.run.app"
+const urlProduct = baseUrl + "/products"
+
+$(document).ready(
+  showData()
+);
+
+function showData() {
+  let tokenData = JSON.parse(sessionStorage.getItem("TOKEN_DATA"));
+  let token = tokenData.token;
+
+  const callData = {
+    "url": urlProduct,
+    "method": "GET",
+    "timeout": 0,
+    "dataType": "json",
+    "headers": {
+      "contentType": "application/json",
+      "Authorization": "Bearer " + token
+    },
+  };
+
+  console.log(callData)
+  $.ajax(callData)
+  .done(function (response) {
+    console.log(response)
+    if (response.status_code == 200) {
+      console.log("tampilkan tabel")
+      tableData(response.data.products)
+    }
+  })
+}
+
+function tableData(apiData) {
+  let tabelData;
+  let noRow;
+  console.log(apiData)
+
+  if(apiData.length == 0){
+    tabelData = `
+    <tr>
+      <td colspan="7">We can't find your request on our Database.</td> 
+    </tr>
+    `;
+  } 
+  else {
+    apiData.map((index, item) =>{
+      noRow = item + 1
+      tabelData += `
+          <tr>
+            <td>${noRow}</td>
+            <td>
+              <a href="./edit.html">
+                <button onClick="editBtn('${index.uuid}')"><i class="fa-solid fa-pen-to-square"></i></button>
+              </a>
+              <a href="#">
+                <button onClick="deleteBtn('${index.uuid}')"><i class="fa-solid fa-trash"></i></button>
+              </a>
+            </td>
+            <td></td>
+            <td class="serial-num"><a href="detail.html">${item.noSerial}</a></td>
+            <td>${item.keypoint}</td>
+            <td>${item.link}</td>
+            <td>${item.pemeliharaanTerakhir}</td>
+          </tr>`
+    })
+  }
+  $("#tableBody").html(tabelData);
+}
+
+//-----------------------------------------fetch api
+// var myHeaders = new Headers();
+// myHeaders.append("Authorization", `Bearer ${token}` );
+
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow',
+//   dataType: 'json'
+// };
+
+// fetch(urlProduct, requestOptions)
+//   .then(response => response.json())
+//   .then((urlProduct) => {
+//     let tableData = `
+//           <tr>
+//             <th scope="col">No</th>
+//             <th scope="col">Action</th>
+//             <th scope="col">QR Code</th>
+//             <th scope="col">No Serial</th>
+//             <th scope="col">Keypoint</th>
+//             <th scope="col">Link</th>
+//             <th scope="col">Pemeliharaan terakhir</th>
+//           </tr>`;
+    
+//     urlProduct.forEach((index) => {
+//       noRow = index+1
+//       tableData += `
+//           <tr>
+//             <td>${index.noRow}</td>
+//             <td>
+//               <a href="./edit.html">
+//               <button onClick="editBtn('${index.uuid}')"><i class="fa-solid fa-pen-to-square"></i></button>
+//               </a>
+//               <a href="#">
+//               <button onClick="deleteBtn('${index.uuid}')"><i class="fa-solid fa-trash"></i></button>
+//               </a>
+//             </td>
+//             <td></td>
+//             <td class="serial-num"><a href="detail.html"></a>${index.noSerial}</td>
+//             <td>${index.keypoint}</td>
+//             <td>${index.link}</td>
+//             <td>${index.pemeliharaanTerakhir}</td>
+//           </tr>`;
+
+//           document.getElementById("tableBody").innerHTML = tableData;
+//     });
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     var callData = new Headers();
+//     callData.append("Authorization", `Bearer ${token}` );
+    
+//     var requestOptions = {
+//       method: 'GET',
+//       headers: callData,
+//       redirect: 'follow'
+//     };
+    
+//     fetch(urlProduct, requestOptions)
+//       .then(response => {
+//         console.log(response)
+//         if (response.status == 200) {
+//           tableData(response.data)
+//         }
+//       }) 
+//       .then(result => {console.log(result))
+//       .catch(error => console.log('error', error));
+// });
+
+// function dataTable()
+
+// async function loadIntoTable(urlProduct, table) {
+//   const tableHead = table.querySelector("thead");
+//   const tableBody = table.querySelector("tbody");
+//   const response = await fetch(urlProduct);
+//   const { headers, rows } = await response.json;
+
+//   // Clear the table
+//   tableHead.innerHTML = "<tr></tr>";
+//   tableBody.innerHTML = "";
+
+//   // Populate the headers
+//   for (const headerText of headers) {
+//     const headerElement = document.createElement("th");
+
+//     headerElement.textContent = headerText;
+//     tableHead.querySelector("tr").appendChild(headerElement);
+//   }
+   
+//   // Populate the rows
+//   for (const row of rows) {
+//     const rowElement = document.createElement("tr");
+
+//     for (const cellText of row) {
+//       const cellElement = document.createElemenr("td");
+
+//       cellElement.textContent = cellText;
+//       rowElement.appendChild(cellElement);
+//     }
+
+//     tableBody.appendChild(rowElement);
+//   }
+
+// }
+
+// loadIntoTable("urlProduct", document.querySelector("table"));
---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const baseUrl = "https://newbackend-datar-zbzgct5ujq-et.a.run.app"
+
+let tmp = JSON.parse(sessionStorage.getItem("INFO_USER"));
+let role = tmp.role;
+console.log(tmp)
+
+$(document).ready(function () {
+    $("#btnLogo").on('click', function (e) {
+        e.preventDefault();
+        btnBack();
+    })
+})
+
+//fungsi untuk button logo navbar
+function btnBack() {
+    if (role=="adminpro") {
+        window.location.href = "index.html"
+    }
+}
+
+//hide feature pada halaman detail untuk role user
+    if (role=='user') {
+        let btn = document.getElementById("btnWrap")
+        btn.style.display = 'none'
+    } 
